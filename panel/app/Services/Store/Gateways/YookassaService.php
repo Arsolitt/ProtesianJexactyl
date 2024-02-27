61729677a44aec30b307cc7eb47d48cce169106a
Arsolitt <arsolitt@gmail.com>
2024-02-27 11:14:36 +0300

    fix: redisign webhook processing workflow

--- panel/app/Services/Store/Gateways/YookassaService.php
+++ panel/app/Services/Store/Gateways/YookassaService.php
@@ -2,9 +2,13 @@
 
 namespace Jexactyl\Services\Store\Gateways;
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Jexactyl\Contracts\Store\PaymentGatewayInterface;
+use Jexactyl\Http\Requests\Api\Webhook\WebhookRequest;
 use Jexactyl\Models\Payment;
+use Jexactyl\Services\Store\PaymentProcessingService;
 
 class YookassaService implements PaymentGatewayInterface
 {
@@ -19,10 +23,13 @@ class YookassaService implements PaymentGatewayInterface
 
     private array $invoice;
 
+    private PaymentProcessingService $processingService;
+
     public function __construct()
     {
         $this->shopID = config('store.gateways.yookassa.shopID');
         $this->secretKey = config('store.gateways.yookassa.secretKey');
+        $this->processingService = new PaymentProcessingService;
     }
 
     public function handle(Payment $payment): Payment
@@ -83,5 +90,73 @@ class YookassaService implements PaymentGatewayInterface
         $this->payment->external_id = $response['id'];
 
         return $this;
+    }
+
+    public function webhook(WebhookRequest $request): Response
+    {
+        if (!$this->checkWebhookSource($request)) {
+            return response('IP is not in whitelist', 403);
+        }
+
+        $data = $request->all()['object']['metadata'];
+        $event = $request->all()['event'];
+        $payment = Payment::findOrFail($data['internal_payment_id']);
+
+        if ($payment->status === 'paid') {
+            return response('Successfully', 200);
+        }
+
+        match ($event) {
+            'payment.succeeded' => $this->processingService->paid($payment),
+            default => $this->processingService->canceled($payment)
+        };
+
+        return response('Successfully', 200);
+    }
+
+    private function checkWebhookSource(WebhookRequest $request): bool
+    {
+        $whitelist = $this->getIPRange();
+
+//        if (!in_array($request->header('CF-Connecting-IP'), $whitelist)) {
+//            Log::error('IP ' . $request->header('CF-Connecting-IP') . ' not in whitelist');
+//            return false;
+//        }
+
+        if (!in_array($request->ip(), $whitelist)) {
+            Log::error('IP ' . $request->ip() . ' not in whitelist');
+            return false;
+        }
+        return true;
+    }
+
+    private function getIPRange(): array
+    {
+        $IPWhitelist = [
+            "185.71.76.0/27",
+            "185.71.77.0/27",
+            "77.75.153.0/25",
+            "77.75.156.11",
+            "77.75.156.35",
+            "77.75.154.128/25",
+            "77.75.154.206",
+            "127.0.0.1",
+            "77.75.153.78",
+            "10.147.19.237",
+        ];
+        $expandedIPs = [];
+        foreach ($IPWhitelist as $item) {
+            if (str_contains($item, '/')) {
+                list($ip, $cidr) = explode('/', $item);
+                $start = ip2long($ip);
+                $end = $start + pow(2, (32 - $cidr)) - 1;
+                for ($i = $start; $i <= $end; $i++) {
+                    $expandedIPs[] = long2ip($i);
+                }
+            } else {
+                $expandedIPs[] = $item;
+            }
+        }
+        return $expandedIPs;
     }
 }
