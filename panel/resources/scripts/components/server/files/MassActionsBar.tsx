42bec350e5e1b7c159dc7620f3ea9bde80d01d9e
Arsolitt <arsolitt@gmail.com>
2024-03-02 17:28:12 +0300

    feat: additional fixes in File Manager

--- panel/resources/scripts/components/server/files/MassActionsBar.tsx
+++ panel/resources/scripts/components/server/files/MassActionsBar.tsx
@@ -33,7 +33,7 @@ const MassActionsBar = () => {
     const onClickCompress = () => {
         setLoading(true);
         clearFlashes('files');
-        setLoadingMessage('Archiving files...');
+        setLoadingMessage('Архивация...');
 
         compressFiles(uuid, directory, selectedFiles)
             .then(() => mutate())
@@ -46,7 +46,7 @@ const MassActionsBar = () => {
         setLoading(true);
         setShowConfirm(false);
         clearFlashes('files');
-        setLoadingMessage('Deleting files...');
+        setLoadingMessage('Удаление...');
 
         deleteFiles(uuid, directory, selectedFiles)
             .then(() => {
@@ -67,16 +67,16 @@ const MassActionsBar = () => {
                     {loadingMessage}
                 </SpinnerOverlay>
                 <Dialog.Confirm
-                    title={'Delete Files'}
+                    title={'Удаление файлов'}
                     open={showConfirm}
-                    confirm={'Delete'}
+                    confirm={'Удалить'}
                     onClose={() => setShowConfirm(false)}
                     onConfirmed={onClickConfirmDeletion}
                 >
                     <p className={'mb-2'}>
-                        Are you sure you want to delete&nbsp;
-                        <span className={'font-semibold text-gray-50'}>{selectedFiles.length} files</span>? This is a
-                        permanent action and the files cannot be recovered.
+                        Ты уверен, что хочешь удалить&nbsp;
+                        <span className={'font-semibold text-gray-50'}>{selectedFiles.length}</span> файлов? Это
+                        необратимое действие и файлы восстановить не получится.
                     </p>
                     {selectedFiles.slice(0, 15).map((file) => (
                         <li key={file}>{file}</li>
@@ -96,11 +96,9 @@ const MassActionsBar = () => {
                     <div className={'fixed bottom-0 mb-6 flex justify-center w-full z-50'}>
                         <Fade timeout={75} in={selectedFiles.length > 0} unmountOnExit>
                             <div css={tw`flex items-center space-x-4 pointer-events-auto rounded p-4 bg-black/50`}>
-                                <Button onClick={() => setShowMove(true)}>Move</Button>
-                                <Button onClick={onClickCompress}>Archive</Button>
-                                <Button.Danger variant={Button.Variants.Secondary} onClick={() => setShowConfirm(true)}>
-                                    Delete
-                                </Button.Danger>
+                                <Button.Text onClick={() => setShowMove(true)}>Переместить</Button.Text>
+                                <Button.Text onClick={onClickCompress}>Архивировать</Button.Text>
+                                <Button.Danger onClick={() => setShowConfirm(true)}>Удалить</Button.Danger>
                             </div>
                         </Fade>
                     </div>
