8bebff110e5f7ceac6db22f84db245aae1d84dbe
Arsolitt <arsolitt@gmail.com>
2024-03-03 20:18:42 +0300

    feat: implement registered events

---
+++ panel/app/Events/User/RegisteredWithReferrer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Jexactyl\Events\User;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Jexactyl\Models\User;
+
+class RegisteredWithReferrer
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(public User $user)
+    {
+        //
+    }
+}
