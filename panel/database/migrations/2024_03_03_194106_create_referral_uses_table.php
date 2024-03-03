5df68cbb34a9063713806368d524b169dfc60207
Arsolitt <arsolitt@gmail.com>
2024-03-03 19:49:40 +0300

    init: create new migrations and models

--- panel/database/migrations/2024_03_03_194106_create_referral_uses_table.php
+++ panel/database/migrations/2024_03_03_194106_create_referral_uses_table.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,10 +15,13 @@ return new class () extends Migration {
         Schema::create('referral_uses', function (Blueprint $table) {
             $table->id();
             $table->text('code_used');
-            $table->integer('user_id');
-            $table->integer('referrer_id');
-            $table->integer('credit_given');
+            $table->unsignedInteger('user_id')->constrained('users')->nullOnDelete();
+            $table->unsignedInteger('referrer_id')->constrained('users')->nullOnDelete();
             $table->timestamps();
+
+            $table->index('code_used');
+            $table->index('user_id');
+            $table->index('referrer_id');
         });
     }
 
