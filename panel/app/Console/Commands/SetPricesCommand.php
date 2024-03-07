b8fc5045aabc4fec8dffd738aefa5148ae71e9de
Arsolitt <arsolitt@gmail.com>
2024-03-07 08:52:54 +0300

    update

---
+++ panel/app/Console/Commands/SetPricesCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Jexactyl\Console\Commands;
+
+use Illuminate\Console\Command;
+use Jexactyl\Models\Server;
+use Symfony\Component\Console\Command\Command as CommandAlias;
+
+class SetPricesCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:actualize-servers';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $servers = Server::all();
+        foreach ($servers as $server) {
+            try {
+                $server->update([
+                    'monthly_price' => $server->actualPrice(),
+                ]);
+            } catch (\Exception $e) {
+                $this->error($e->getMessage());
+            }
+        };
+        return CommandAlias::SUCCESS;
+    }
+}
