6f67d506db0da26fe9eb7c0c6fe1a88b92212f55
Arsolitt <arsolitt@gmail.com>
2024-03-07 11:32:32 +0300

    update

--- panel/app/Http/Requests/Admin/Jexactyl/AdvancedFormRequest.php
+++ panel/app/Http/Requests/Admin/Jexactyl/AdvancedFormRequest.php
@@ -12,22 +12,22 @@ class AdvancedFormRequest extends AdminFormRequest
     public function rules(): array
     {
         return [
-            'jexactyl:auth:2fa_required' => 'required|integer|in:0,1,2',
+            'auth:2fa_required' => 'required|integer|in:0,1,2',
 
             'recaptcha:enabled' => 'required|in:true,false',
             'recaptcha:secret_key' => 'required|string|max:191',
             'recaptcha:website_key' => 'required|string|max:191',
-            'jexactyl:guzzle:timeout' => 'required|integer|between:1,60',
-            'jexactyl:guzzle:connect_timeout' => 'required|integer|between:1,60',
+            'guzzle:timeout' => 'required|integer|between:1,60',
+            'guzzle:connect_timeout' => 'required|integer|between:1,60',
 
-            'jexactyl:client_features:allocations:enabled' => 'required|in:true,false',
-            'jexactyl:client_features:allocations:range_start' => [
+            'client_features:allocations:enabled' => 'required|in:true,false',
+            'client_features:allocations:range_start' => [
                 'nullable',
                 'required_if:jexactyl:client_features:allocations:enabled,true',
                 'integer',
                 'between:1024,65535',
             ],
-            'jexactyl:client_features:allocations:range_end' => [
+            'client_features:allocations:range_end' => [
                 'nullable',
                 'required_if:jexactyl:client_features:allocations:enabled,true',
                 'integer',
