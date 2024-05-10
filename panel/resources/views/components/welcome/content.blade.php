aec9742c3316b5c903df44d1237d8d549b82443f
Arsolitt <arsolitt@gmail.com>
2024-05-10 10:20:42 +0300

    update

--- panel/resources/views/components/welcome/content.blade.php
+++ panel/resources/views/components/welcome/content.blade.php
@@ -3,11 +3,11 @@
 ])
 
 <div class="flex min-h-full flex-1">
-    <div class="flex flex-col md:grid grid-cols-3 grid-rows-3 text-center font-bold text-lg flex-1 content-center">
-        <div class="cell order-2 md:order-0"><p class="welcome-button">Низкий пинг из любой точки мира</p></div>
-        <div class="cell order-4 md:order-0"><p class="welcome-button">Без свапа и оверсела</p></div>
-        <div class="cell order-1 md:order-0"><p class="welcome-button">Админ знает толк в серверах и кубиках</p></div>
-        <div class="cell order-3 md:order-0"><p class="welcome-button">Гибкая конфигурация</p></div>
+    <div class="grid grid-cols-3 grid-rows-3 text-center font-bold text-lg flex-1 content-center">
+        <div class="cell"><p class="welcome-button">Низкий пинг из любой точки мира</p></div>
+        <div class="cell"><p class="welcome-button">Без свапа и оверсела</p></div>
+        <div class="cell"><p class="welcome-button">Админ знает толк в серверах и кубиках</p></div>
+        <div class="cell"><p class="welcome-button">Гибкая конфигурация</p></div>
 
         <div class="cell">
             <p class="xs:text-3xl myblur rounded-xl p-1">
@@ -19,10 +19,10 @@
             </p>
         </div>
 
-        <div class="cell order-6 md:order-0"><p class="welcome-button">Защита от DDOS атак</p></div>
-        <div class="cell order-7 md:order-0"><p class="welcome-button">Ежедневные бэкапы</p></div>
-        <div class="cell order-8 md:order-0"><p class="welcome-button">Дней работы: {{ $operationTime }}</p></div>
-        <div class="cell order-5 md:order-0"><p class="welcome-button">SLA 99% доступности</p></div>
+        <div class="cell"><p class="welcome-button">Защита от DDOS атак</p></div>
+        <div class="cell"><p class="welcome-button">Ежедневные бэкапы</p></div>
+        <div class="cell"><p class="welcome-button">Дней работы: {{ $operationTime }}</p></div>
+        <div class="cell"><p class="welcome-button">SLA 99% доступности</p></div>
 
     </div>
 </div>
@@ -33,12 +33,5 @@
         align-items: center;
         justify-content: center;
         margin-bottom: 10px;
-    }
-    .welcome-button {
-
-    }
-
-    .welcome-button:hover {
-
     }
 </style>
